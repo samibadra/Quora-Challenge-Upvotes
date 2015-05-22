ef6deb108e5ab9c3f761f535a3ca7db6fffd38f2
samibadra <samibadra@yahoo.com>
2015-05-21 20:36:31 -0700

    Update upvotes.cpp

--- upvotes.cpp
+++ upvotes.cpp
@@ -1,6 +1,17 @@
 /*
- Copyright 2015 Sami Badra
+ Sami Badra
  
+ May 16, 2015
+ Quora Challenge (Upvotes)
+ FILE: upvotes.java
+ 
+ Challenege description may be found in this GitHub Repository,
+ or by visiting www.quora.com/challenges
+ 
+ Copyright (c) 2015 Sami Badra. All rights reserved.
+ */
+
+/*
  DISCLAIMER: Any unauthorized use, including but not limited to, copying or
  redistributing any chunk of the source code (or an entire file) will result in
  punishment by law. I, Sami Badra,own all rights to the files and their contents.
@@ -8,12 +19,6 @@
  P.S. This source code has already been submitted to Quora, so it is in your best
  interest NOT to copy ANY of the source code you see here. Quora checks for code
  similarities from previously submitted version.You will be noticed and punished.
- 
- upvotes.cpp
- Quora_challenge_upvotes
- 
- Prompt description found at www.quora.com/challenges
- Challenge Title: Upvotes
  */
 
 #include <iostream>
